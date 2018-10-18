95f114a0d04c83e59a1ab4f014cdc0bdcc4f807e
DineshS <DineshS@DINESHS01.emids.local>
2018-10-18 12:35:30 +0530

    First Commit

---
+++ src/com/dinesh/sample/App.java
@@ -0,0 +1,10 @@
+package com.dinesh.sample;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey! This is  Dinesh Here!");
+	}
+
+}
