e66ee024b01873c9e141fd7e60347aaf732d8762
DineshS <DineshS@DINESHS01.emids.local>
2018-10-18 13:02:18 +0530

    converting the project to maven and adding the spring boot dependencies.

--- src/com/dinesh/sample/App.java
+++ src/com/dinesh/sample/App.java
@@ -1,10 +1,15 @@
 package com.dinesh.sample;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hey! This is  Dinesh Here!");
+		 SpringApplication.run(App.class, args);
 	}
 
 }
