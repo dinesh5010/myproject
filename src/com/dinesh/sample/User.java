82cd7c7121734f645ab14ee9d0ce57d0e156bd0a
DineshS <DineshS@DINESHS01.emids.local>
2018-10-18 13:07:57 +0530

    adding the User class

---
+++ src/com/dinesh/sample/User.java
@@ -0,0 +1,27 @@
+package com.dinesh.sample;
+
+public class User {
+	
+	private String name;
+	private int rollNumber;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
